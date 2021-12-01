9bc98657ef2fc15dcc3c4eb922ba24281df81c07
Mohan2401 <mohan.chandran@syncfusion.com>
2021-12-01 18:53:27 +0530

    ReadOnly sample committed into the examples.

---
+++ XlsIO-Excel-Protect-UnProtect/ReadOnly/ReadOnly/Program.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.XlsIO;
+using System;
+using System.IO;
+
+namespace ReadOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+
+                FileStream input = new FileStream("../../../Data/InputData.xlsx", FileMode.Open, FileAccess.ReadWrite);
+
+                //Open Excel
+                IWorkbook workbook = application.Workbooks.Open(input);
+
+                //Set Read only
+                workbook.ReadOnlyRecommended = true;
+
+                FileStream output = new FileStream("../../../Output/ReadOnlyOutput.xlsx", FileMode.Create, FileAccess.ReadWrite);
+
+                //Save the document
+                workbook.SaveAs(output);
+            }
+        }
+    }
+}
