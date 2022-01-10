0269d76d4654e018368ff2bf5b0b537a249e652a
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2022-01-10 13:13:40 +0530

    WF-62162-UGSamples

---
+++ Worksheet Rows and Columns Manipulation/Expand-Collapse-Groups/Expand-Collapse-Groups/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using Expand_Collapse_Groups.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Syncfusion.XlsIO;
+using System.IO;
+
+namespace Expand_Collapse_Groups.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ExpandGroups()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream fileStream = new FileStream("InputTemplate - To Expand.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(fileStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                #region Expand Groups
+                //Expand row groups
+                worksheet.Range["A3:A7"].ExpandGroup(ExcelGroupBy.ByRows, ExpandCollapseFlags.ExpandParent);
+                worksheet.Range["A11:A16"].ExpandGroup(ExcelGroupBy.ByRows);
+
+                //Expand column groups
+                worksheet.Range["C1:D1"].ExpandGroup(ExcelGroupBy.ByColumns, ExpandCollapseFlags.ExpandParent);
+                worksheet.Range["F1:G1"].ExpandGroup(ExcelGroupBy.ByColumns);
+                #endregion
+
+                //Saving the workbook
+                MemoryStream stream = new MemoryStream();
+                workbook.SaveAs(stream);
+
+                //Set the position as '0'.
+                stream.Position = 0;
+
+                //Download the Excel file in the browser
+                FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/excel");
+                fileStreamResult.FileDownloadName = "ExpandGroups.xlsx";
+                return fileStreamResult;
+            }
+        }
+        public IActionResult CollapseGroups()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream fileStream = new FileStream("InputTemplate - To Collapse.xlsx", FileMode.Open, FileAccess.Read);
+                IWorkbook workbook = application.Workbooks.Open(fileStream);
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                #region Collapse Groups
+                //Collapse row groups
+                worksheet.Range["A3:A7"].CollapseGroup(ExcelGroupBy.ByRows);
+                worksheet.Range["A11:A16"].CollapseGroup(ExcelGroupBy.ByRows);
+
+                //Collapse column groups
+                worksheet.Range["C1:D1"].CollapseGroup(ExcelGroupBy.ByColumns);
+                worksheet.Range["F1:G1"].CollapseGroup(ExcelGroupBy.ByColumns);
+                #endregion
+
+                //Saving the workbook
+                MemoryStream stream = new MemoryStream();
+                workbook.SaveAs(stream);
+
+                //Set the position as '0'.
+                stream.Position = 0;
+
+                //Download the Excel file in the browser
+                FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/excel");
+                fileStreamResult.FileDownloadName = "CollapseGroups.xlsx";
+                return fileStreamResult;
+            }
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Worksheet Rows and Columns Manipulation/Expand-Collapse-Groups/Expand-Collapse-Groups/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Expand_Collapse_Groups.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
